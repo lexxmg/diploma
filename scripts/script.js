90d9ed68a9b64a3a6e8ae85f052031e376be8481
lexxmg <63001174+lexxmg@users.noreply.github.com>
2020-04-12 16:37:48 +0300

    js-action

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,9 @@ $(function() {
         callBtn = $('.js-call'),
         askBtn = $('.js-ask'),
         orderBtn = $('.js-order'),
-        modal = $('.js-modal'),
+        submitBtn = $('.js-submit'),
+        form = $('.modal__form'),
+        modal = $('.js-modal')
         modalMail = $('.js-modal__mail'),
         modalText = $('.js-modal__text');
 
@@ -51,21 +53,49 @@ $(function() {
     body.css('overflow', 'hidden');
   });
 
+  function modalClose(){
+    modal.css('display', 'none');
+    body.css('overflow', 'auto');
+
+    if(modalMail.css('display') == 'block') {
+      modalMail.css('display', 'none');
+    }
+
+    if(modalText.css('display') == 'block') {
+      modalText.css('display', 'none');
+    }
+
+    $('.form__input[name=tel]').val('');
+    $('.form__input[name=nik]').val('')
+  } 
+
   modal.on('click', function(event){
     if (event.target == this) {
-      modal.css('display', 'none');
-      body.css('overflow', 'auto');
-
-      if(modalMail.css('display') == 'block') {
-        modalMail.css('display', 'none');
-      }
-
-      if(modalText.css('display') == 'block') {
-        modalText.css('display', 'none');
-      }
+      modalClose();
     }
   });
 
+  form.on('submit', function(event){
+    event.preventDefault();
+    console.log($(this).serialize());
+    console.log($(this).serializeArray()[1].value);
+    console.log($('.form__input[name=tel]').val());
+    console.log($('.form__input[name=nik]').val());
+
+    $.get(
+      $(this).attr('action'),
+      $(this).serialize(),
+      function(data){
+        if(data == '') {
+          alert('Ошибка сервера, повторите попытку позже');
+          modalClose();
+        } else {
+          alert(data);
+          modalClose();
+        }  
+      }
+    )
+  });
   /*
   body.on('click', function(event){
     if(btn.attr("aria-expanded") == "true") {
